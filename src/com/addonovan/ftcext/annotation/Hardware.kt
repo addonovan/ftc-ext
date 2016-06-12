a1e5483856241b8452c84bc9d48b1f232cd39de9
addonovan <addonovan@users.noreply.github.com>
2016-06-12 16:48:45 -0500

    Added @Hardware annotation
    @Hardware is used to annotation properties or fields that represent
    a piece of hardware (i.e. a DcMotor or Servo, etc.) that were
    configured by the device mapping.
    
    By using this annotation, when the OpMode is initialized, the value
    of the Hardware device will be grabbed from the hardware map and
    set as the value of the property or field.

--- src/com/addonovan/ftcext/annotation/Hardware.kt
+++ src/com/addonovan/ftcext/annotation/Hardware.kt
@@ -1,6 +1,14 @@
 package com.addonovan.ftcext.annotation
 
 /**
+ * An annotation placed on a property or field that represents
+ * a piece of hardware that's configured with a name in the
+ * hardware map.
+ *
  * @author addonovan
  * @since 6/12/16
  */
+@Target( AnnotationTarget.PROPERTY, AnnotationTarget.FIELD )
+@Retention( AnnotationRetention.RUNTIME )
+@MustBeDocumented
+annotation class Hardware( val name: String = "" )
