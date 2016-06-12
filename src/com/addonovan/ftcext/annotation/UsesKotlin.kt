dba2bb8b56a7a8ab210086497b1723534edf9a14
addonovan <addonovan@users.noreply.github.com>
2016-06-12 16:37:49 -0500

    Basic OpMode class & @UsesKotlin
    The OpMode class overrides the init() method from it's superclass,
    and prevents subclasses from overriding it again; however, its
    functionality has been replaced with the new abstract method
    `initRobot()`, which is automatically called at the end of the
    init() method.
    
    Because reflections in Kotlin is slightly different than it is
    in Java, we need OpModes that are written in Kotlin to have the
    @UsesKotlin annotation on the class so the API will be able to
    adapt and perform the correct tasks.

---
+++ src/com/addonovan/ftcext/annotation/UsesKotlin.kt
@@ -0,0 +1,12 @@
+package com.addonovan.ftcext.annotation
+
+/**
+ * An control that flags if a class is written in Kotlin or not.
+ *
+ * @author addonovan
+ * @since 6/12/16
+ */
+@Target( AnnotationTarget.ANNOTATION_CLASS )
+@Retention( AnnotationRetention.RUNTIME )
+@MustBeDocumented
+annotation class UsesKotlin;
---
+++ src/com/addonovan/ftcext/control/OpMode.kt
@@ -0,0 +1,59 @@
+package com.addonovan.ftcext.control
+
+import com.addonovan.ftcext.annotation.UsesKotlin
+
+/**
+ * The OpMode class built upon Qualcomm's. This class provides more utility
+ * than the standard class does, and _must_ be used in order to be able
+ * to access the other functionality of this library.
+ *
+ * @author addonovan
+ * @since 6/12/16
+ */
+abstract class OpMode : com.qualcomm.robotcore.eventloop.opmode.OpMode()
+{
+
+    //
+    // Initializations
+    //
+
+    /**
+     * The final override of the init method.
+     * We need this in our abstract class to get basic things done that will
+     * make the subclass(es) easier to use.
+     */
+    final override fun init()
+    {
+        initRobot(); // perform regular initialization
+    }
+
+    //
+    // Abstracts
+    //
+
+    /**
+     * Initializes the robot as the `init()` method would in the standard
+     * `OpMode` class.
+     */
+    abstract fun initRobot();
+
+    abstract override fun loop(); // just here so it's easier to tell what the subclass needs to override
+
+}
+
+/**
+ * Determines if the given class uses Kotlin or not.
+ *
+ * @param[class]
+ *          The class to test for the `@UsesKotlin` control.
+ * @return If `class` has the `@UsesKotlin` control on it.
+ */
+private fun isKotlin( `class`: Class< out OpMode > ): Boolean
+{
+    for ( annotation in `class`.annotations )
+    {
+        if ( annotation is UsesKotlin ) return true;
+    }
+
+    return false;
+}
