e2ab9538d518be67deaacc994d5a9fd0d40e6a63
addonovan <addonovan@users.noreply.github.com>
2016-06-12 21:30:46 -0500

    HardwareConfigurer can configure java opmodes
    HardwareConfigurer can go through java files and pull out the fields
    annotated with @Hardware and configured them from their correct
    hardware maps

--- library/src/main/java/com/addonovan/ftcext/control/HardwareConfigurer.kt
+++ library/src/main/java/com/addonovan/ftcext/control/HardwareConfigurer.kt
@@ -3,6 +3,7 @@ package com.addonovan.ftcext.control
 import com.addonovan.ftcext.*
 import com.addonovan.ftcext.annotation.Hardware
 import com.addonovan.ftcext.reflection.FieldFinder
+import com.qualcomm.robotcore.hardware.HardwareMap
 import java.lang.reflect.Field
 import java.lang.reflect.Modifier
 import java.util.*
@@ -20,8 +21,11 @@ class HardwareConfigurer( val opMode: OpMode, private val isKotlin: Boolean )
     // Vals
     //
 
-    /** The fields for a nonKotlin OpMode */
-    private val fields: ArrayList< Field > = ArrayList();
+    /** The fields (if non-Kotlin) that are marked with @Hardware to be configured. */
+    private val fieldFinder: FieldFinder?;
+
+    /** The fields for the hardware maps that we can access via the [OpMode.hardwareMaps] object. */
+    private val hardwareMaps: ArrayList< Field >;
 
     //
     // Constructor
@@ -29,7 +33,7 @@ class HardwareConfigurer( val opMode: OpMode, private val isKotlin: Boolean )
 
     init
     {
-        i( "" );
+        i( "Configuring Hardware for ${getRegisterName( opMode )}" );
 
         if ( isKotlin )
         {
@@ -41,13 +45,13 @@ class HardwareConfigurer( val opMode: OpMode, private val isKotlin: Boolean )
             // must have @Hardware
             // must be public AND static
             // must be neither final NOR abstract
-            val hardwareFields = FieldFinder( opMode ).with( Hardware::class.java )
+            fieldFinder = FieldFinder( opMode ).with( Hardware::class.java )
                                     .`is`( Modifier.STATIC )
-                                    .isNot( Modifier.FINAL, Modifier.ABSTRACT, lenient = false )
-                                    .get();
-
-            fields.addAll( hardwareFields ); // add all the hardware fields
+                                    .isNot( Modifier.FINAL, Modifier.ABSTRACT, lenient = false );
         }
+
+        // hardware maps are found the same way
+        hardwareMaps = FieldFinder( opMode.hardwareMap ).inheritsFrom( HardwareMap.DeviceMapping::class.java ).get();
     }
 
     //
@@ -62,7 +66,31 @@ class HardwareConfigurer( val opMode: OpMode, private val isKotlin: Boolean )
         }
         else
         {
-            
+            // the only reason I make this assertion is because I don't want to use
+            // the nasty-ass ?. operators later on.
+            if ( fieldFinder == null )
+            {
+                wtf( "fieldFinder == null! This should NEVER be true if this is a java file!" );
+                wtf( "Unable to configure hardware devices because of this!" );
+                return;
+            }
+
+            // loop over all the hardware maps we have
+            hardwareMaps.forEach { field ->
+
+                // get the device mapping from the list
+                val map = field.get( opMode.hardwareMap ) as HardwareMap.DeviceMapping< * >;
+
+                // find only the fields that match the generic type of the DeviceMapping
+                // (we duplicate() so that we can reuse the fieldFinder for other types)
+                val fields = fieldFinder.duplicate().inheritsFrom( getGenericType( map ) ).get();
+
+                // with all of our fields of this type, set the value of the field
+                // to what this map says the value for the name of the field is
+                fields.forEach { field ->
+                    field.set( opMode, map[ getName( field ) ] );
+                };
+            }
         }
     }
 
@@ -71,16 +99,10 @@ class HardwareConfigurer( val opMode: OpMode, private val isKotlin: Boolean )
      */
     private fun getName( field: Field ): String
     {
-        val annotation = field.getAnnotation( Hardware::class.java );
+        val name = getHardwareName( field );
 
-        if ( annotation.name.isNullOrBlank() )
-        {
-            return field.name;
-        }
-        else
-        {
-            return annotation.name;
-        }
+        return if ( name.isNullOrBlank() ) field.name;
+               else                        name;
     }
 
 }
