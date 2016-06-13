c08aca1df71238c8fa1aebd24786219aa2a19a38
addonovan <addonovan@users.noreply.github.com>
2016-06-12 20:31:30 -0500

    The start of the dirty, dirty hacks file
    Don't look at it if you want to see code that is reasonable.

--- library/src/main/java/com/addonovan/ftcext/Hacks.kt
+++ library/src/main/java/com/addonovan/ftcext/Hacks.kt
@@ -10,18 +10,22 @@ import android.util.Log
  * @since 6/12/16
  */
 
+// ahhh
+// the sweet sound of suppressed warnings
+// I sure do love my suppression!
+
 // All Logcat commands are now just methods of every object!
-fun Any.v( tag: String, data: String ) = Log.v( tag, data );
-fun Any.d( tag: String, data: String ) = Log.d( tag, data );
-fun Any.i( tag: String, data: String ) = Log.i( tag, data );
-fun Any.w( tag: String, data: String ) = Log.w( tag, data );
-fun Any.e( tag: String, data: String ) = Log.e( tag, data );
-fun Any.wtf( tag: String, data: String ) = Log.wtf( tag, data );
+@Suppress( "unused" ) fun Any.v( tag: String, data: String ) = Log.v( tag, data );
+@Suppress( "unused" ) fun Any.d( tag: String, data: String ) = Log.d( tag, data );
+@Suppress( "unused" ) fun Any.i( tag: String, data: String ) = Log.i( tag, data );
+@Suppress( "unused" ) fun Any.w( tag: String, data: String ) = Log.w( tag, data );
+@Suppress( "unused" ) fun Any.e( tag: String, data: String ) = Log.e( tag, data );
+@Suppress( "unused" ) fun Any.wtf( tag: String, data: String ) = Log.wtf( tag, data );
 
 // assumes it's an ftcext item and just grabs the class name and makes that the tag
-fun Any.v( data: String ) = Log.v( "ftcext.${javaClass.simpleName}", data );
-fun Any.d( data: String ) = Log.d( "ftcext.${javaClass.simpleName}", data );
-fun Any.i( data: String ) = Log.i( "ftcext.${javaClass.simpleName}", data );
-fun Any.w( data: String ) = Log.w( "ftcext.${javaClass.simpleName}", data );
-fun Any.e( data: String ) = Log.e( "ftcext.${javaClass.simpleName}", data );
-fun Any.wtf( data: String ) = Log.wtf( "ftcext.${javaClass.simpleName}", data );
+@Suppress( "unused" ) fun Any.v( data: String ) = Log.v( "ftcext.${javaClass.simpleName}", data );
+@Suppress( "unused" ) fun Any.d( data: String ) = Log.d( "ftcext.${javaClass.simpleName}", data );
+@Suppress( "unused" ) fun Any.i( data: String ) = Log.i( "ftcext.${javaClass.simpleName}", data );
+@Suppress( "unused" ) fun Any.w( data: String ) = Log.w( "ftcext.${javaClass.simpleName}", data );
+@Suppress( "unused" ) fun Any.e( data: String ) = Log.e( "ftcext.${javaClass.simpleName}", data );
+@Suppress( "unused" ) fun Any.wtf( data: String ) = Log.wtf( "ftcext.${javaClass.simpleName}", data );
