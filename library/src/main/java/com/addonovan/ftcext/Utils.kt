8b2a3f8e7db146f7082be17b513b2982062a9c45
addonovan <addonovan@users.noreply.github.com>
2016-06-16 21:51:17 -0500

    Fixed alert() added spinnerDialog()
    alert was apparently creating a spinner dialog, so I changed the method
    to actually do what I wanted it to do.
    
    spinnerDialog creates a dialog where the user can choose between multiple
    strings.

--- library/src/main/java/com/addonovan/ftcext/Utils.kt
+++ library/src/main/java/com/addonovan/ftcext/Utils.kt
@@ -2,6 +2,8 @@ package com.addonovan.ftcext
 
 import android.app.AlertDialog
 import android.content.DialogInterface
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.addonovan.ftcext.control.Register
 
 /**
@@ -18,14 +20,9 @@ import com.addonovan.ftcext.control.Register
 /**
  * @param[clazz]
  *          The class with a [Register] annotation.
- * @return The value of the [Register.name], or `null` if the annoation isn't present.
+ * @return The value of the [Register.name].
  */
-fun getRegisterName( clazz: Class< * > ): String?
-{
-    if ( !clazz.isAnnotationPresent( Register::class.java ) ) return null;
-
-    return clazz.getAnnotation( Register::class.java ).name;
-}
+fun getRegisterName( clazz: Class< * > ) = clazz.getAnnotation( Register::class.java ).name;
 
 //
 // Android
@@ -38,13 +35,19 @@ fun getRegisterName( clazz: Class< * > ): String?
  *          The title of the dialog.
  * @param[message]
  *          The message of the dialog.
+ * @param[positive]
+ *          The text for the positive button. Default is "Yes"
+ * @param[neutral]
+ *          The text for the neutral button (null for none). Default is `null`
+ * @param[negative]
+ *          The text for the negative button. Default is "No"
  * @param[onClick]
  *          The function to invoke when an option is chosen, the parameter is the
  *          text of the chosen button.
- * @param[options]
- *          The dialog buttons (by default "Cancel" and "Set").
  */
-fun alert( title: String, message: String, onClick: ( String ) -> Unit, vararg options: String = arrayOf( "Cancel", "Set" ) )
+fun alert( title: String, message: String,
+           positive: String = "Yes", neutral: String? = null, negative: String = "No",
+           onClick: ( String ) -> Unit )
 {
     // the basics of the window
     val builder = AlertDialog.Builder( Context );
@@ -52,12 +55,42 @@ fun alert( title: String, message: String, onClick: ( String ) -> Unit, vararg o
     builder.setMessage( message );
 
     // set up the buttons
-    builder.setItems( options, { dialog, which ->
-        onClick.invoke( options[ which ] );
-    } );
+    builder.setNegativeButton( negative, { dialog, which -> onClick( negative ) } );
+    builder.setPositiveButton( positive, { dialog, which -> onClick( positive ) } );
+    if ( neutral != null )
+    {
+        builder.setNeutralButton( neutral, { dialog, which -> onClick( neutral ) } );
+    }
 
     // don't let the user cancel it, they have to choose a button
     builder.setCancelable( false );
+    builder.show();
+}
 
+/**
+ * Creates a spinner dialog where the user chooses from multiple items.
+ *
+ * @param[title]
+ *          The title of the dialog.
+ * @param[message]
+ *          The message of the dialog.
+ * @param[items]
+ *          An array of the text of the items for the spinner.
+ * @param[onClick]
+ *          The function to invoke when an option is chosen, the parameter is the
+ *          text of the chosen button.
+ */
+fun spinnerDialog( title: String, message: String,
+                   items: Array< String >,
+                   onClick: ( String ) -> Unit )
+{
+    // the basics of the window
+    val builder = AlertDialog.Builder( Context );
+    builder.setTitle( title );
+    builder.setMessage( message );
+
+    builder.setItems( items, { dialog, which -> onClick( items[ which ] ) } );
+
+    builder.setCancelable( false );
     builder.show();
 }
