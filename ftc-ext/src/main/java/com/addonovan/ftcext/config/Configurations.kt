ebcb6cc2541ce4491959c65bbe4d3fc8859a16d9
addonovan <addonovan@users.noreply.github.com>
2016-08-17 19:13:58 -0500

    :Added Configurations object
    Configurations is essentially a container of all the OpModeConfigs and
    also saves and loads the configuration file.

---
+++ ftc-ext/src/main/java/com/addonovan/ftcext/config/Configurations.kt
@@ -0,0 +1,157 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Austin Donovan (addonovan)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.addonovan.ftcext.config
+
+import android.os.Environment
+import android.util.JsonWriter
+import com.addonovan.ftcext.*
+import org.json.JSONObject
+import java.io.*
+import java.util.*
+
+/**
+ * !Description!
+ *
+ * Serializes to
+ * ```json
+ * root: [
+ *   {
+ *     name: "name",
+ *     activeProfile: "activeProfileName",
+ *     profiles: [
+ *       {
+ *         name: "name",
+ *         config: [
+ *           [ "name", type, value ],
+ *           ...
+ *         ]
+ *       },
+ *       ...
+ *     ]
+ *   },
+ *   ...
+ * ]
+ * ```
+ *
+ * @author addonovan
+ * @since 8/17/16
+ */
+object Configurations : Jsonable, ILog by getLog( Configurations::class )
+{
+
+    //
+    // Values
+    //
+
+    /** The location of the config file on the disk. */
+    private val ConfigFile = File( Environment.getExternalStorageDirectory(), "/FIRST/profiles.ftcext" );
+
+    /** A map of the OpModeConfigs for each OpMode. */
+    val OpModeConfigs = HashMap< String, OpModeConfig >();
+
+    //
+    // Serialization
+    //
+
+    /**
+     * Creates [ConfigFile] if it needs to be.
+     *
+     * @return `true` if the file was created, `false` otherwise.
+     */
+    private fun createFileIfNeeded(): Boolean
+    {
+        if ( !ConfigFile.exists() )
+        {
+            e( "Config file doesn't exist!" );
+
+            if ( !ConfigFile.parentFile.exists() )
+            {
+                i( "Making directory to file" );
+                ConfigFile.parentFile.mkdirs();
+            }
+
+            ConfigFile.createNewFile();
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Loads the configurations from the configuration file.
+     */
+    fun load()
+    {
+        if ( createFileIfNeeded() )
+        {
+            OpModeConfigs.clear();
+            return;
+        }
+
+        i( "Loading profiles from config file" );
+        fromJson( JSONObject( ConfigFile.readText() ) );
+    }
+
+    /**
+     * Saves the configurations to the configuration file.
+     */
+    fun save()
+    {
+        createFileIfNeeded();
+
+        i( "Saving profiles to config file" );
+        val writer = JsonWriter( OutputStreamWriter( FileOutputStream( ConfigFile ) ) );
+        toJson( writer );
+        writer.close();
+    }
+
+    override fun toJson( writer: JsonWriter )
+    {
+        writer.name( "root" ).beginArray();
+        OpModeConfigs.forEach { it.value.toJson( writer ); };
+        writer.endArray();
+    }
+
+    /**
+     * Reads the json object and populates [OpModeConfigs] to
+     * resemble it.
+     *
+     * @param[json]
+     *          The json to parse.
+     */
+    fun fromJson( json: JSONObject )
+    {
+        // clear any previous configs
+        OpModeConfigs.clear();
+
+        // load everything from the list
+        val root = json.getJSONArray( "root" );
+        for ( i in 0..root.length() - 1 )
+        {
+            val omConfig = OpModeConfig.fromJson( root.getJSONObject( i ) );
+            OpModeConfigs[ omConfig.Name ] = omConfig;
+        }
+    }
+
+}
